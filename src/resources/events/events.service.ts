d8c87fa588d011b0de4336c9481da08089a30bf1
Dutra PC <joao.vitornl@gmail.com>
2024-01-11 21:40:22 -0300

    :construction: Create events functions

--- src/resources/events/events.service.ts
+++ src/resources/events/events.service.ts
@@ -1,42 +1,60 @@
-import jwt, { Secret }from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
-
 import { Event, EventUpdate } from "./dtos/events.dtos.ts";
-import userModel from "../../models/user.ts";
+import eventModel from '../../models/event.ts';
 
 export default class Service {
-    async create({  } : Event): Promise<any> {
+    async create({ description, consultant, startDate, endDate, name, client} : Event): Promise<any> {
       try {
-         
+         const event = new eventModel({
+            description,
+            consultant,
+            startDate,
+            endDate,
+            client,
+            name,
+         })
+         await event.save();
+
+         return event;
       } catch (err) {
         return { error: 'internal_error' };
       }
     }
-    async get({ authorization, id }: EventUpdate): Promise<any> {
+    async get({ id }: EventUpdate): Promise<any> {
       try {
-       
+        const findEvent = await eventModel.findById(id);
+        if (!findEvent) return { error: "event_not_found"};
+
+        return { event: findEvent }
       } catch (err) {
         return { error: 'internal_error' };
       }
     }
     async getAll({ authorization }: EventUpdate): Promise<any> {
       try {
-       
+        return await eventModel.find().sort({date: -1});
       } catch (err) {
         return { error: 'internal_error' };
       }
     }
     async update({ id, data }: EventUpdate): Promise<any> {
       try {
-      
+        const findEvent = await eventModel.findById(id);
+        if (!findEvent) return { error: "event_not_found"};
 
+        const event = await eventModel.findByIdAndUpdate(id, { $set: { ...data }}, { new: true});
+        return { event }; 
       } catch (err) {
         return { error: 'internal_error' };
       }
     }
     async delete({ id }: EventUpdate): Promise<any> {
       try {
-      
+        const findEvent = await eventModel.findById(id);
+        if (!findEvent) return { error: "event_not_found"};
+
+        await eventModel.findByIdAndDelete(id);
+        return {}; 
+
       } catch (err) {
         return { error: 'internal_error' };
       }
